5ad86dfbc252fc88b2a570112407671177e87d92
sevvaltiraslar <sevvaltiraslar@gmail.com>
2021-11-06 19:14:01 +0300

    test

---
+++ Ecommerce/CategoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Ecommerce
+//
+//  Created by SEVVAL on 5.11.2021.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
